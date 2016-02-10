94f44b347bdc21a3a5a99f3ca8c9229dbcc8d0b6
Piotr Kuźnik <piotr.damian.kuznik@gmail.com>
2016-02-10 11:06:48 +0100

    initall
    initall

---
+++ src/FastUnitTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace FastUnitTest;
+
+/**
+ * Description of FastUnitTest
+ * 
+ * @package FastUnitTest
+ * @version 0.1
+ * @author pkuznik
+ * @license http://gnu.org/copyleft/gpl.html GNU
+ * @copyright (c) eDokumenty Sp. z o.o
+ */
+class FastUnitTest {
+    //put your code here
+}
